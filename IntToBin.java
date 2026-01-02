f1abcb2ab0e86461fd6caf414a661f73a5efe9ab
Lior Perlin <lior.perlin@post.runi.ac.il>
2026-01-02 23:58:24 +0200

    Fixed bug in IntToBin

--- IntToBin.java
+++ IntToBin.java
@@ -8,8 +8,8 @@ public class IntToBin {
 
     /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
     public static String toBinary(int x) {
-        if(x == 0) { // Base condition - we got to the end of the number
-            return "";
+        if(x <= 1) { // Base condition - we got to the end of the number
+            return x + "";
         }
         if ((x % 2) == 0) { //That means our current bin number is devidable by 2 so it should recieve a '0'
             return toBinary(x / 2) + "0";
